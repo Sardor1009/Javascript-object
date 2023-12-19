8f8be856359754704dfb9a93fa2c6c7f30e39814
Sardor1009 <islomovsardor1009@gmail.com>
2023-12-19 22:27:40 +0500

    first

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let person = {
+    firstName: "Abdulaziz",
+    lastName: "Toshpulatov",
+    age: 30,
+    subjects: ["math", "physics", "history"],
+    isMarried: false,
+    isDiplom: NaN,
+    test: undefined,
+      2019: "Enter to the university",
+    2011: "Olympiads first position",
+    university: {
+      name: "TATU",
+      course: 4,
+      GPA: 4.72,
+    },
+    symbol: Symbol(),
+    fullName: function () {
+      return "Abdulaziz Toshpulatov";
+    },
+  };
